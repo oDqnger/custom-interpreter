6fcc5487d84d3127d461fb9062873a86184104d1
oDqnger <dimanchansilu@gmail.com>
2024-12-29 13:31:00 +0000

    got started with reading the file and removing whitespaces from it

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::env;
+
+fn remove_whitespace(code: String) -> String {
+    let mut formatted_code = String::new();
+    for ch in code.chars() {
+        if !ch.is_whitespace() {
+            formatted_code.push(ch);
+        }
+    }
+
+    formatted_code
 }
+
+fn read_file() -> String {
+    let args : Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    return contents
+}
+
+fn main() { 
+    let code = remove_whitespace(read_file());
+    
+    for c in code.chars() {
+         
+    }
+
+    println!("{}", code);
+}
+
