2e26b2667ecff557ccb2ed422304478ccc959e87
oDqnger <dimanchansilu@gmail.com>
2024-12-29 14:03:28 +0000

    made a simple lexer

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,6 +1,60 @@
 use std::fs;
 use std::env;
 
+#[derive(Debug)]
+
+enum TokenTypes {
+    Integer,
+    PlusSign,
+    MinusSign,
+    MultiplySign,
+    DivisionSign,
+    EOL,
+}
+
+struct Lexer {
+    token: TokenTypes,
+    value: String,
+}
+
+impl Lexer {
+    fn identify_token(c: char) -> Result<Self, String> {
+        if c.is_digit(10) {
+            Ok(Self {
+                token: TokenTypes::Integer,
+                value: c.to_string(),
+            })
+        } else if c == '+' {
+            Ok(Self {
+                token: TokenTypes::PlusSign,
+                value: c.to_string(),
+            })
+        } else if c == '-' {
+            Ok(Self {
+                token: TokenTypes::MinusSign,
+                value: c.to_string(),
+            })
+        } else if c == '*' {
+            Ok(Self {
+                token: TokenTypes::MultiplySign,
+                value: c.to_string(),
+            })
+        } else if c == '/' {
+            Ok(Self {
+                token: TokenTypes::DivisionSign,
+                value: c.to_string(),
+            })
+        } else if c == ';' {
+            Ok(Self {
+                token: TokenTypes::EOL,
+                value: c.to_string(),
+            })
+        } else {
+            Err("I DONT UDNERSTAND".to_string())
+        }
+    }
+}
+
 fn remove_whitespace(code: String) -> String {
     let mut formatted_code = String::new();
     for ch in code.chars() {
@@ -22,9 +76,10 @@ fn read_file() -> String {
 
 fn main() { 
     let code = remove_whitespace(read_file());
-    
+    let mut lexer;
     for c in code.chars() {
-         
+        lexer = Lexer::identify_token(c).unwrap();
+        println!("{:?}", lexer.token);
     }
 
     println!("{}", code);
